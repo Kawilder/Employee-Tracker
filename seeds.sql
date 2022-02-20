00407efbec5a9dd27eb48b1c49ae29648609e8e5
Kawilder <kevin_wilde564@yahoo.com>
2022-02-19 19:48:53 -0600

    inital commit + schema file

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+use eeTracker_db
+
+INSERT INTO department (name)
+  VALUE ("shipping"),
+  ("factory"),
+  ("HR"),
+  ("grind"),
+  ("paint"),
+  ("assembly"),
+  ("shipping");
+
+INSERT INTO role (title, salary, department_id)
+  VALUE ("driver", 35000.00, )
