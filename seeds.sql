f910eeccb91efdcde645f06feda2581005cbdbac
Kawilder <kevin_wilde564@yahoo.com>
2022-02-20 09:37:54 -0600

    Half made seed. finish it later

--- seeds.sql
+++ seeds.sql
@@ -1,13 +1,21 @@
 use eeTracker_db
 
 INSERT INTO department (name)
-  VALUE ("shipping"),
-  ("factory"),
+  VALUE ("Shipping"),
+  ("Factory"),
   ("HR"),
-  ("grind"),
-  ("paint"),
-  ("assembly"),
-  ("shipping");
+  ("Finance");
 
 INSERT INTO role (title, salary, department_id)
-  VALUE ("driver", 35000.00, )
+  VALUE ("Forklift-Operator", 65000.00, 1),
+  ("Shipping-Manager", 75000.00, 1),
+  ("Operator-1", 65000.00, 2),
+  ("Operator-2", 70000.00, 2),
+  ("Shift-Lead", 80000.00, 2),
+  ("Shift-Supervisor", 900000.00, 2),
+  ("Human-Resources", 65000.00, 2),
+  ("Accountant", 50000.00, 4),               
+  ("Senior Accountant", 80000.00, 4),    
+
+INSERT INSERT employee (first_name, last_name, role_id, manager_id)
+
